5049d7671f4b29066fd79e72497c514ea05c499b
Sureddi, Manoj (ESI) <manoj.sureddi@adp.com>
2017-08-09 21:15:57 +0530

    Added boggle to github

---
+++ globals.js
@@ -0,0 +1,13 @@
+const globals={
+        // variable for total score
+    totalScore: 0,
+    currentWord: [],
+    // var last selected, current selected, all valid selections 
+    lastSelection: undefined,
+    currentSelection: 0,
+    score: 0,
+    validSelections: [],
+    containsQ: false,
+    selectionArrays: [],
+    selectionArrays: []
+}
